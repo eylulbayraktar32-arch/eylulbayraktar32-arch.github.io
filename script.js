f564baede9471ec9825968179834d8a598a0e665
eylulbayraktar32-arch <eylulbayraktar32@gmail.com>
2025-12-16 18:22:26 +0300

    Update script.js

--- script.js
+++ script.js
@@ -4,12 +4,12 @@ document.addEventListener('DOMContentLoaded', function() {
         { title: "Gece Yarısı Kütüphanesi", author: "Matt Haig", price: "90 ₺", condition: "Çok İyi", img: "https://i.dr.com.tr/cache/600x600-0/originals/0001922926001-1.jpg" },
         { title: "Rezonans Kanunu", author: "Pierre Franckh", price: "75 ₺", condition: "İyi", img: "https://i.dr.com.tr/cache/600x600-0/originals/0001801769001-1.jpg" },
         { title: "Engereğin Gözü", author: "Zülfü Livaneli", price: "85 ₺", condition: "Çok İyi", img: "https://i.dr.com.tr/cache/600x600-0/originals/0001902371001-1.jpg" },
-        { title: "Atomik Alışkanlıklar", author: "James Clear", price: "95 ₺", condition: "Mükemmel", img: "https://i.dr.com.tr/cache/600x600-0/originals/0001983750001-1.jpg" },
-        { title: "Küçük Prens", author: "Antoine de Saint-Exupéry", price: "50 ₺", condition: "Çok İyi", img: "https://i.dr.com.tr/cache/600x600-0/originals/0000000689023-1.jpg" },
-        { title: "Kürk Mantolu Madonna", author: "Sabahattin Ali", price: "65 ₺", condition: "Çok İyi", img: "https://i.dr.com.tr/cache/600x600-0/originals/0000000058245-1.jpg" },
-        { title: "1984", author: "George Orwell", price: "80 ₺", condition: "İyi", img: "https://i.dr.com.tr/cache/600x600-0/originals/0001789098001-1.jpg" },
-        { title: "Simyacı", author: "Paulo Coelho", price: "70 ₺", condition: "Çok İyi", img: "https://i.dr.com.tr/cache/600x600-0/originals/0000000064552-1.jpg" },
-        { title: "Sapiens", author: "Yuval Noah Harari", price: "130 ₺", condition: "Mükemmel", img: "https://i.dr.com.tr/cache/600x600-0/originals/0000000633872-1.jpg" }
+        { title: "Yüzleşmenin 40 Kuralı", author: "Elif Şafak", price: "80 ₺", condition: "Mükemmel", img: "https://i.dr.com.tr/cache/600x600-0/originals/0000000359077-1.jpg" },
+        { title: "Şeker Portakalı", author: "José Mauro de Vasconcelos", price: "60 ₺", condition: "Çok İyi", img: "https://i.dr.com.tr/cache/600x600-0/originals/0000000064101-1.jpg" },
+        { title: "İnsan Ne ile Yaşar", author: "Lev Tolstoy", price: "55 ₺", condition: "İyi", img: "https://i.dr.com.tr/cache/600x600-0/originals/0000000064552-1.jpg" },
+        { title: "Kayıp Gül", author: "Serdar Özkan", price: "70 ₺", condition: "Mükemmel", img: "https://i.dr.com.tr/cache/600x600-0/originals/0000000359077-1.jpg" },
+        { title: "Beyaz Zambaklar Ülkesinde", author: "Grigory Petrov", price: "65 ₺", condition: "Çok İyi", img: "https://i.dr.com.tr/cache/600x600-0/originals/0000000633872-1.jpg" },
+        { title: "Kürk Mantolu Madonna", author: "Sabahattin Ali", price: "65 ₺", condition: "Çok İyi", img: "https://i.dr.com.tr/cache/600x600-0/originals/0000000058245-1.jpg" }
     ];
 
     const container = document.getElementById('books-container');
@@ -44,6 +44,7 @@ function openModal(title, author, price, condition, img) {
     const modal = new bootstrap.Modal(document.getElementById('bookModal'));
     modal.show();
 }
+
 
 
 
