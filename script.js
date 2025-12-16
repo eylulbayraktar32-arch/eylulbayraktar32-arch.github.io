3e2bde485b5074b58b6806a6b09ef9a8e2405670
eylulbayraktar32-arch <eylulbayraktar32@gmail.com>
2025-12-16 17:02:11 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ document.addEventListener('DOMContentLoaded', function() {
     const books = [
         { title: "1984", author: "George Orwell", price: "85 ₺", condition: "Çok İyi", img: "https://m.media-amazon.com/images/I/71kxaO1Qf-L._AC_UF1000,1000_QL80_.jpg" },
         { title: "Simyacı", author: "Paulo Coelho", price: "70 ₺", condition: "İyi", img: "https://m.media-amazon.com/images/I/61FdWtwanqL._AC_UF894,1000_QL80_.jpg" },
-        { title: "Sapiens", author: "Yuval Noah Harari", price: "120 ₺", condition: "Mükemmel", img: "https://www.ynharari.com/wp-content/uploads/2017/01/sapiens.png" },
+        { title: "Sapiens", author: "Yuval Noah Harari", price: "120 ₺", condition: "Mükemmel", img: "https://m.media-amazon.com/images/I/41VNuQf+CUL._AC_UF1000,1000_QL80_.jpg" },
         { title: "Küçük Prens", author: "Antoine de Saint-Exupéry", price: "50 ₺", condition: "Çok İyi", img: "https://m.media-amazon.com/images/I/71jG+NT4+3L._AC_UF1000,1000_QL80_.jpg" },
         { title: "Dune", author: "Frank Herbert", price: "110 ₺", condition: "İyi", img: "https://m.media-amazon.com/images/I/81kYgT5yrJL._AC_UF1000,1000_QL80_.jpg" },
-        { title: "Atomik Alışkanlıklar", author: "James Clear", price: "95 ₺", condition: "Mükemmel", img: "https://m.media-amazon.com/images/I/51-nXsSRfZL._AC_UF1000,1000_QL80_.jpg" },
+        { title: "Atomik Alışkanlıklar", author: "James Clear", price: "95 ₺", condition: "Mükemmel", img: "https://m.media-amazon.com/images/I/513qwXn862L._AC_UF1000,1000_QL80_.jpg" },
         { title: "Körlük", author: "José Saramago", price: "80 ₺", condition: "Çok İyi", img: "https://m.media-amazon.com/images/I/81fJXB2R-JL.jpg" },
         { title: "Nutuk", author: "Mustafa Kemal Atatürk", price: "60 ₺", condition: "İyi", img: "https://m.media-amazon.com/images/I/51yH-3o4WKL.jpg" },
         { title: "Harry Potter ve Felsefe Taşı", author: "J.K. Rowling", price: "100 ₺", condition: "Mükemmel", img: "https://m.media-amazon.com/images/I/71-++hbbERL._AC_UF1000,1000_QL80_.jpg" },
@@ -34,4 +34,5 @@ document.addEventListener('DOMContentLoaded', function() {
         container.appendChild(col);
     });
 });
+
 
